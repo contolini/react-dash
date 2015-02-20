2620b1a2238af75d29001e75d95b780d9697ce23
Irina Muchnik <irina.muchnik@cfpb.gov>
2015-02-20 00:14:19 -0500

    Fixed common test

--- js/components/__tests__/App-test.js
+++ js/components/__tests__/App-test.js
@@ -1,16 +1,16 @@
-/** @jsx React.DOM */
-
 // __tests__/App-test.js
 
+/** @jsx React.DOM */
+jest.dontMock('../../common.js');
 
-var common = require('../../common');
+describe('Get all teams method', function () {
+    it('should call a callback', function () {
 
-describe('Get all teams method', function() {
-  it('should call a callback', function() {
-    var promise = common.getAllTeams();
-    console.log(promise);
-    promise.done(function( result ) {
-      expect( result ).toBeDefined();
+        var common = require('../../common'),
+            $ = require('jquery');
+
+        common.getAllTeams();
+
+        expect($.get).toBeCalled();
     });
-  });
 });
